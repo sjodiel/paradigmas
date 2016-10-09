1cff0ae8bdc762c6ad5dd36d23607842999fd374
sjodiel <jodiel_santos@hotmail.com>
2016-10-08 21:45:41 -0300

    Update/Create Files

---
+++ src/br/ufma/deinf/lista01/Exe01.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufma.deinf.lista01;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Scanner;
+//import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+/**
+ *
+ * @author sjodiel
+ */
+public class Exe01 {
+    
+    public static void main(String[] args) {
+        
+        int km, litros;
+        int totalKm =0;
+        int totalLitros=0;
+        float kmPorLitros,totalKmPorLitros;
+        
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Informe a quilometragem: ");
+        km = input.nextInt();
+        
+        //System.out.println("");
+        //litros = input.nextInt();
+        
+        DecimalFormat formatar = new DecimalFormat("#.##");
+        
+        
+        while( km != -1){
+            
+            System.out.println("Informe os litros: ");
+            litros = input.nextInt();
+            
+            totalKm += km;
+            totalLitros += litros;
+            
+            if (litros !=0){
+                
+                kmPorLitros = (float) km/litros;
+                
+                
+                System.out.println("Consumo em km/litros eh " + formatar.format(kmPorLitros));
+                System.out.println("");
+                
+            
+            }
+            
+            totalKmPorLitros = (float) totalKm/totalLitros;
+            
+            System.out.println("Total de km/litros: " + formatar.format(totalKmPorLitros));
+            System.out.println("");
+            
+            System.out.println("Informe a quilometragem: ");
+            km = input.nextInt();
+     
+           
+        
+        }
+        
+        
+        
+    }
+    
+}
---
+++ src/br/ufma/deinf/lista01/Exe03.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufma.deinf.lista01;
+
+/**
+ *
+ * @author sjodiel
+ */
+public class Exe03 {
+    
+    public static void main(String[] args) {
+        
+        int horasTrabalhadas;
+        int numEmpregados;
+        int salarioHora;
+        double salarioBruto;
+        
+        
+        
+    }
+}

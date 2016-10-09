de90842a115c67db4cd77a298e30572c999c9ecf
sjodiel <jodiel_santos@hotmail.com>
2016-10-09 10:09:26 -0300

    Add exe06

---
+++ src/br/ufma/deinf/lista01/exe06/LetraA.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufma.deinf.lista01.exe06;
+
+/**
+ *
+ * @author sjodiel
+ */
+public class LetraA {
+
+//@@@@@ $$$$$ &&&&&
+    public static void main(String[] args) {
+        int y,x;
+        
+        y=8;
+        x=5;        
+        
+        if (y == 8)
+            
+            if (x == 5)//{
+                System.out.println("@@@@@");
+            //}
+            else//{
+                        System.out.println("#####");
+            //}
+        
+        System.out.println("$$$$$");
+        System.out.println("&&&&&");
+        
+           
+        
+    }
+
+}

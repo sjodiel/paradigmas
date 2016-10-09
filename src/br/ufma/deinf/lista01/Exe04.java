94e8f2a17c1fefeb58778d796600c9785a8fc758
sjodiel <jodiel_santos@hotmail.com>
2016-10-09 10:07:57 -0300

    Add Exe04

---
+++ src/br/ufma/deinf/lista01/Exe04.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufma.deinf.lista01;
+
+/**
+ *
+ * @author sjodiel
+ */
+public class Exe04 {
+    
+    public static void main(String[] args) {
+    
+        System.out.println("N\t10*N\t100*N\t1000*N");
+        
+        for(int n=1;n<=5;n++){
+            
+            System.out.println(n + "\t" + 10*n + "\t" + 100*n +"\t"+1000*n);
+            
+        }
+    }
+    
+}

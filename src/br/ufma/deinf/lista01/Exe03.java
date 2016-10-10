84fc7ecdb610a54cc93b5354ed2a392a8328b5d2
sjodiel <jodiel_santos@hotmail.com>
2016-10-09 21:50:15 -0300

    Update exe03

--- src/br/ufma/deinf/lista01/Exe03.java
+++ src/br/ufma/deinf/lista01/Exe03.java
@@ -5,20 +5,43 @@
  */
 package br.ufma.deinf.lista01;
 
+import java.util.Scanner;
+
 /**
  *
  * @author sjodiel
  */
 public class Exe03 {
-    
+
     public static void main(String[] args) {
-        
+
         int horasTrabalhadas;
-        int numEmpregados;
+        int empregado;
         int salarioHora;
-        double salarioBruto;
-        
-        
-        
+        //double salarioBruto;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Informe as horas trabalhadas:");
+        horasTrabalhadas = input.nextInt();
+        System.out.println("Informe o salario hora:");
+        salarioHora = input.nextInt();
+
+        if (horasTrabalhadas <= 40) {
+
+            empregado = (horasTrabalhadas * salarioHora);
+
+        } else {
+
+            empregado = 40 * salarioHora;
+            empregado += (horasTrabalhadas - 40) * (salarioHora * 1.5);
+            //emp[i] += (h[i]-40)*(s[i]*1.5);
+
+        }
+
+        System.out.println("\nEmpregado:");
+        System.out.println("Salario-hora: " + salarioHora);
+        System.out.println("Horas trabalhadas: " + horasTrabalhadas);
+        System.out.println("Salário Final: " + empregado);
+
     }
 }

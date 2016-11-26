8ad830d35dfa262a1358a7b4a03ba2b4d47845e1
sjodiel <jodiel_santos@hotmail.com>
2016-11-26 12:29:55 -0300

    Update

--- src/br/ufma/deinf/lista01/Exe01.java
+++ src/br/ufma/deinf/lista01/Exe01.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ufma.deinf.lista01;
+package br.ufma.deinf.lista01;                  
 
 import java.text.DecimalFormat;
 import java.util.Scanner;

aad862be3b9658cd0e9ab2917ec33b58b27e5f1b
sjodiel <jodiel_santos@hotmail.com>
2016-11-26 12:33:50 -0300

    Update lista 02 exe01
    Add data and teste

---
+++ src/br/ufma/deinf/lista02/exe01/Data.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufma.deinf.lista02.exe01;
+
+/**
+ *
+ * @author sjodiel
+ */
+public class Data {
+    
+}
